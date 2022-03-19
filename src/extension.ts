e606d2752ef688f9d439c0d6caa2e54821376634
gaoliang <gaoliangim@gmail.com>
2022-03-19 14:42:06 +0800

    feat: support content edit view

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,15 @@ import { ZkNode } from './ZkNode';
 import ZkPreviewProvider from './ZkPreviewProvider';
 import { ZooKeeperProvider } from './ZooKeeperProvider';
 import * as zkClient from './ZkClient';
+import { MemFS } from './zkFileSystemProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const memFs = new MemFS();
+	context.subscriptions.push(vscode.workspace.registerFileSystemProvider('memfs', memFs, { isCaseSensitive: true }));
+
 	const readOnlyScheme = 'zk-readonly';
 	let zkPreviewProvider = new ZkPreviewProvider();
 	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(readOnlyScheme, zkPreviewProvider));
@@ -54,11 +59,48 @@ export function activate(context: vscode.ExtensionContext) {
 			zkClient.createClient(server);
 		}
 	}));
-	
+
+	context.subscriptions.push(vscode.commands.registerCommand('visualZooKeeper.editNode', async (node: ZkNode) => {
+
+		if (!zkClient.client || !zkClient.isConnected) {
+			vscode.window.showErrorMessage(`[Visual ZooKeeper]: faild to view node content, server is not connected!`, 'Configure ZK Server').then(
+				(value) => {
+					if (value === 'Configure ZK Server') {
+						vscode.commands.executeCommand('visualZooKeeper.configureServer');
+					}
+				}
+			);
+			return;
+		}
+		// two source: tree item click or cmmand input. 
+		let path;
+		if (node) {
+			path = node.fullPath;
+		} else {
+			path = await vscode.window.showInputBox({ placeHolder: 'ZooKeeper node path' });
+		}
+
+		if (path) {
+			// that a trick: use U+29f8 "⧸" replace U+002f / slash. and put file at root /.
+			let filePath = '/' + path.split("/").join("⧸");
+			const uri = vscode.Uri.parse('memfs:' + filePath);
+			// create file in memfs for temp save.
+			zkClient.client?.getData(
+				path,
+				function (error: any, data: any, stat: any) {
+					memFs.writeFile(uri, data, { create: true, overwrite: true });
+					vscode.commands.executeCommand("vscode.open", uri);
+				}
+			);
+
+		} else {
+			vscode.window.showInformationMessage('[Visual ZooKeeper] node path is empty');
+		}
+	}));
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "visual-zookeeper" is now active!');
-
 }
 
 // this method is called when your extension is deactivated
