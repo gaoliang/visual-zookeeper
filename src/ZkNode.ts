ce9fb2b1af47d2c790ab88bcdad1da4f3976eb03
gaoliang <gaoliangim@gmail.com>
2022-03-19 18:02:42 +0800

    feat: copy path

--- src/ZkNode.ts
+++ src/ZkNode.ts
@@ -1,4 +1,7 @@
+import path = require('path');
 import * as vscode from 'vscode';
+import { ThemeIcon } from 'vscode';
+import { zkPathToFileName } from './zkFileSystemProvider';
 export class ZkNode extends vscode.TreeItem {
     constructor(
         public readonly label: string,
@@ -9,6 +12,8 @@ export class ZkNode extends vscode.TreeItem {
         super(label, collapsibleState);
         this.tooltip = `${this.label}-${this.description}`;
         this.description = this.description;
+        this.resourceUri = vscode.Uri.parse("zkfs:/" + zkPathToFileName(this.fullPath));
+        // this.iconPath = new ThemeIcon("circle-filled");
     }
     contextValue = "zkNode";
 }
--- src/extension.ts
+++ src/extension.ts
@@ -115,6 +115,11 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}));
 
+	context.subscriptions.push(vscode.commands.registerCommand('visualZooKeeper.copyPath', async (node: ZkNode) => {
+		vscode.env.clipboard.writeText(node.fullPath);
+		vscode.window.showInformationMessage('[Visual ZooKeeper] Successfully write path to clipboard');
+	}));
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "visual-zookeeper" is now active!');
